ebc01ac375e949c0763f9b88b6e3a9124d923ce6
Shirley Wang <shirleywang527@gmail.com>
2019-08-22 13:07:28 +0200

    minter wonderland style

---
+++ MinterWonderland.js
@@ -0,0 +1,12 @@
+
+var tlMinter = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750
+});
+
+tlMinter.add({
+    targets: '.minter-wonderland h1',
+    opacity: 1,
+    scale: 1.2,
+    delay: anime.stagger(500)
+})
