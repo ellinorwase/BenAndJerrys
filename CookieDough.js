606cf3b9a2f3494459a9caa8fd689d3abbac86e3
Ellinore <ellinor.wase@gmail.com>
2019-08-22 12:01:03 +0200

    bug-fix

--- CookieDough.js
+++ CookieDough.js
@@ -1,27 +1,63 @@
 let clickFunction = document.getElementById("click-function");
-clickFunction.onclick = function(){
+clickFunction.addEventListener("mouseenter", event => {
+  var animation = anime({
+    targets: "img.cookie-dough-pic",
+    scale: 1.4,
+    rotate: "1turn"
+  })
 
-    
+  function loop(t) {
+    animation.tick(t);
+    customRAF = requestAnimationFrame(loop);
+  }
+
+  requestAnimationFrame(loop);
+});
+clickFunction.addEventListener("mouseleave", event => {
     var animation = anime({
-        targets: 'img.cookie-dough-pic',
-        translateY: [
-            {value: 200, duration: 500},
-            {value: 0, duration: 500},
-        ],
-        rotate: {
-            value: "1turn",
-            easing: "easeInOutSine"
-        },
-        easing: 'easeInOutQuad',
-        autoplay: false,
-        direction: 'alternate',
-
+      targets: "img.cookie-dough-pic",
+      scale: 1,
+      rotate: "2turn"
     });
-    
+  
     function loop(t) {
-        animation.tick(t);
-        customRAF = requestAnimationFrame(loop);
+      animation.tick(t);
+      customRAF = requestAnimationFrame(loop);
     }
-    
+  
     requestAnimationFrame(loop);
-}
+  });
+
+// Cookie Dough Text
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector(
+  ".cookie-dough-heading .cookie-dough-letters"
+);
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /([^\x00-\x80]|\w)/g,
+  "<span class='cookie-dough-letters'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".cookie-dough-heading .cookie-dough-letters",
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: function(el, i) {
+      return 100 * i;
+    }
+  })
+  .add({
+    targets: ".cookie-dough-heading",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  })
+//   .add({
+//     targets: ".color-rgb",
+//     color: 'rgb(255,255,255)', 
+//   });
+//   .add({ targets: ".color-rgb", color: "rgb(58, 106, 209)"},0);
