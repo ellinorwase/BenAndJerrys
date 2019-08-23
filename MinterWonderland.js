5eb9375641a5ef937bbf85a2ffcf59157619570d
Shirley Wang <shirleywang527@gmail.com>
2019-08-23 07:23:15 +0200

    failed attempt

--- MinterWonderland.js
+++ MinterWonderland.js
@@ -9,3 +9,31 @@ tlMinter.add({
     scale: 1.2,
     delay: anime.stagger(500)
 })
+
+// function scrollInVisible() {
+//     const headers = document.querySelectorAll('.section-header')
+//     headers.forEach(header => {
+//         if (isElementInViewport(header)) {
+//             header.classList.add("header-visible");
+//         } else {
+//             header.classList.remove("header-visible");
+//         }
+//     })
+// }
+
+// function isElementInViewport (el) {
+//     var rect = el.getBoundingClientRect();
+
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
+
+// if (window.addEventListener) {
+//     addEventListener('DOMContentLoaded', scrollInVisible, false);
+//     addEventListener('load', scrollInVisible, false);
+//     addEventListener('scroll', scrollInVisible, false);
+// }
