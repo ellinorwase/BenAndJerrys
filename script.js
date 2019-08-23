3bda9142846e3b06a44df9ba0bf8c02cdefa1af0
Shirley Wang <shirleywang527@gmail.com>
2019-08-23 10:40:18 +0200

    add highlight animation with js

--- script.js
+++ script.js
@@ -42,3 +42,26 @@ const rotateIt = anime({
 document.querySelector('.icon').addEventListener('mouseover', () => {
     rotateIt.play()
 })
+
+// HIGHLIGHT FOLLOWS
+const triggers = document.querySelectorAll('em');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlightLinks() {
+    const linkCoords = this.getBoundingClientRect();
+
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX
+    };
+
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+
+triggers.forEach(em => em.addEventListener('mouseenter', highlightLinks));
