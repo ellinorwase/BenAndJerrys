5e43d7a147c5596eb7a4843f9efd20822d404141
Shirley Wang <shirleywang527@gmail.com>
2019-08-23 08:27:01 +0200

    add a couple of animation to minter wonderland

--- MinterWonderland.js
+++ MinterWonderland.js
@@ -1,39 +1,15 @@
 var tlMinter = anime.timeline({
     easing: 'easeOutExpo',
-    duration: 750
+    duration: 1500
 });
 
 tlMinter.add({
     targets: '.minter-wonderland h1',
     opacity: 1,
-    scale: 1.2,
+    scale: 1.3,
     delay: anime.stagger(500)
 })
 
-// function scrollInVisible() {
-//     const headers = document.querySelectorAll('.section-header')
-//     headers.forEach(header => {
-//         if (isElementInViewport(header)) {
-//             header.classList.add("header-visible");
-//         } else {
-//             header.classList.remove("header-visible");
-//         }
-//     })
-// }
-
-// function isElementInViewport (el) {
-//     var rect = el.getBoundingClientRect();
-
-//     return (
-//         rect.top >= 0 &&
-//         rect.left >= 0 &&
-//         rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//     );
-// }
-
-// if (window.addEventListener) {
-//     addEventListener('DOMContentLoaded', scrollInVisible, false);
-//     addEventListener('load', scrollInVisible, false);
-//     addEventListener('scroll', scrollInVisible, false);
-// }
+document.querySelector('.minter-wonderland-pic').addEventListener('click', () => {
+    tlMinter.play()
+})
